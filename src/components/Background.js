a2a04c95679c03a011de9b6221c3961b8f2aade7
beurkinger <tgoehringer@gmail.com>
2017-04-03 18:55:39 +0200

    removed second buffer

--- src/components/Background.js
+++ src/components/Background.js
@@ -2,8 +2,10 @@ import Inferno, { linkEvent } from 'inferno';
 import Component from 'inferno-component';
 
 const GRID_SIZE = 33;
+const STROKE_SIZE = 10;
 const MAX_DROP_SIZE = 10;
 const MAX_DEVIATION = 8;
+const PAINT_COLOR = 'lightblue';
 
 class Background extends Component {
 
@@ -12,19 +14,19 @@ class Background extends Component {
 
     this.canvas = null;
     this.ctx = null;
-    this.buffers = {back : null, front : null}
+    this.buffer = null;
 
     this.props = { mouseX : -1, mouseY : -1 };
 
     this.paint = this.paint.bind(this);
+    this.drip = this.drip.bind(this);
     this.handleResize = this.handleResize.bind(this);
 	}
 
 
   componentDidMount () {
-    // this.canvas.style.background = 'black';
     this.setCanvasDimensions();
-    this.createBuffers();
+    this.createBuffer();
     this.ctx = this.canvas.getContext("2d");
     this.ctx.imageSmoothingEnabled = false;
     this.clear();
@@ -33,7 +35,7 @@ class Background extends Component {
   }
 
   shouldComponentUpdate (nextProps) {
-    this.paint(this.props, nextProps);
+    if (this.props.mouseX !== -1 && this.props.mouseY !== -1) this.paint(this.props, nextProps);
     return false;
   }
 
@@ -43,10 +45,9 @@ class Background extends Component {
     this.canvas.height = document.documentElement.clientHeight;
   }
 
-  createBuffers () {
-    // this.buffers.back = new Buffer(this.canvas.width, this.canvas.height);
-    this.buffers.front = new Buffer(this.canvas.width, this.canvas.height);
-    this.drawGrid(this.buffers.front);
+  createBuffer () {
+    this.buffer = new Buffer(this.canvas.width, this.canvas.height);
+    this.drawGrid(this.buffer);
   }
 
   drawGrid (buffer) {
@@ -57,75 +58,76 @@ class Background extends Component {
     buffer.ctx.fillStyle = '#FFF';
     buffer.ctx.fill();
     buffer.ctx.closePath();
-    return;
-    buffer.ctx.lineWidth = 1;
-    buffer.ctx.strokeStyle = '#000';
-    buffer.ctx.beginPath();
 
-    for (let i = 0; i < nbSquaresX; i++) {
-      buffer.ctx.moveTo(i * GRID_SIZE, 0);
-      buffer.ctx.lineTo(i * GRID_SIZE, buffer.canvas.height);
-      buffer.ctx.stroke();
-    }
-    for (let i = 0; i < nbSquaresY; i++) {
-      buffer.ctx.moveTo(0, i * GRID_SIZE);
-      buffer.ctx.lineTo(buffer.canvas.width, i * GRID_SIZE);
-      buffer.ctx.stroke();
-    }
+    // buffer.ctx.lineWidth = 1;
+    // buffer.ctx.strokeStyle = '#000';
+    // buffer.ctx.beginPath();
+    //
+    // for (let i = 0; i < nbSquaresX; i++) {
+    //   buffer.ctx.moveTo(i * GRID_SIZE, 0);
+    //   buffer.ctx.lineTo(i * GRID_SIZE, buffer.canvas.height);
+    //   buffer.ctx.stroke();
+    // }
+    // for (let i = 0; i < nbSquaresY; i++) {
+    //   buffer.ctx.moveTo(0, i * GRID_SIZE);
+    //   buffer.ctx.lineTo(buffer.canvas.width, i * GRID_SIZE);
+    //   buffer.ctx.stroke();
+    // }
 
   }
 
   paint (oldProps, nextProps) {
     requestAnimationFrame(() => {
+      this.buffer.ctx.globalCompositeOperation = 'destination-out'
+      this.buffer.ctx.lineWidth = STROKE_SIZE;
+      this.buffer.ctx.strokeStyle = PAINT_COLOR;
+      this.buffer.ctx.lineCap = 'round';
+
+      this.buffer.ctx.beginPath();
+      this.buffer.ctx.moveTo(oldProps.mouseX, oldProps.mouseY);
+      this.buffer.ctx.lineTo(nextProps.mouseX, nextProps.mouseY);
+      this.buffer.ctx.stroke();
+      this.refresh();
+    });
+  }
+
+  drip () {
+    requestAnimationFrame(() => {
       let radius = Math.random() * MAX_DROP_SIZE;
       let xDev = parseInt(Math.random() * MAX_DEVIATION) - MAX_DEVIATION / 2 - 1;
       let yDev = parseInt(Math.random() * MAX_DEVIATION) - MAX_DEVIATION / 2 - 1;
-      let color = 'lightblue';
-      this.buffers.front.ctx.globalCompositeOperation = 'destination-out'
+      this.buffer.ctx.globalCompositeOperation = 'destination-out'
+      this.buffer.ctx.fillStyle = PAINT_COLOR;
 
-      console.log(oldProps.mouseX);
-      console.log(nextProps.mouseX);
-      this.buffers.front.ctx.beginPath();
-      this.buffers.front.ctx.moveTo(oldProps.mouseX, oldProps.mouseY);
-      this.buffers.front.ctx.lineTo(nextProps.mouseX, nextProps.mouseY);
-      this.buffers.front.ctx.lineWidth = MAX_DROP_SIZE;
-      this.buffers.front.ctx.strokeStyle = color;
-      this.buffers.front.ctx.lineCap = 'round';
-      this.buffers.front.ctx.stroke();
-
-
-
-      this.refresh();
-      return;
-      console.log('yo');
-      this.buffers.front.ctx.beginPath();
-      this.buffers.front.ctx.globalCompositeOperation = 'destination-out'
-      this.buffers.front.ctx.arc(this.props.mouseX + xDev, this.props.mouseY + yDev, radius, 0, Math.PI * 2, true);
-      // this.buffers.front.ctx.rect(this.props.mouseX + xDev, this.props.mouseY + yDev, radius, radius);
-
-      this.buffers.front.ctx.fillStyle = color;
-      this.buffers.front.ctx.fill();
+      this.buffer.ctx.beginPath();
+      this.buffer.ctx.arc(this.props.mouseX + xDev, this.props.mouseY + yDev, radius, 0, Math.PI * 2, true);
+      this.buffer.ctx.fill();
       this.refresh();
     });
   }
 
   clear() {
     this.setCanvasDimensions();
-    this.buffers.front.setSize(this.canvas.width, this.canvas.height);
-    this.drawGrid(this.buffers.front);
+    this.buffer.setSize(this.canvas.width, this.canvas.height);
+    this.drawGrid(this.buffer);
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.ctx.drawImage(this.buffers.front.canvas, 0, 0);
+    this.ctx.drawImage(this.buffer.canvas, 0, 0);
   }
 
   refresh () {
+    // let size = MAX_DROP_SIZE * 2 + MAX_DEVIATION * 2;
+    // let x = this.props.mouseX - size / 2;
+    // let y = this.props.mouseY - size / 2 ;
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.drawImage(this.buffer.canvas, 0, 0);
+  }
+
+  smartRefresh () {
     let size = MAX_DROP_SIZE * 2 + MAX_DEVIATION * 2;
     let x = this.props.mouseX - size / 2;
     let y = this.props.mouseY - size / 2 ;
-    // this.ctx.clearRect(x, y, size, size);
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.ctx.drawImage(this.buffers.front.canvas, 0, 0);
-
-    // this.ctx.drawImage(this.buffers.front.canvas, x, y, size, size, x, y, size, size);
+    this.ctx.drawImage(this.buffer.canvas, 0, 0);
   }
 
   handleResize () {
